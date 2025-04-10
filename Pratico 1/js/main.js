889d26d42bd2531aa3a9dca0ce2487aed69e83f9
00Karim <karimveleza@gmail.com>
2025-04-10 13:48:46 -0300

    mejora en organizacion de archivos, avance en secion de contactos y funcion de cambiar entre distintos formularios de contacto con js

---
+++ Pratico 1/js/main.js
@@ -0,0 +1,4 @@
+const formulario1 = document.querySelector(".contacto1")
+const formulario2 = document.querySelector(".contacto2")
+formulario1.style.display = "none";
+formulario2.style.removeProperty("display")
